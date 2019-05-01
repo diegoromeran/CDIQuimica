103836317b778d1e0edcf5389a396f01a31d143c
guillermoiglesiashernandez <guillermo.iglesias.hernandez@alumnos.upm.es>
2019-05-01 13:34:36 +0200

    hecho el hidrogeno beta

---
+++ Assets/Scripts/electronMov.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class electronMov : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hola mundo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f,22f,0f));
+        transform.Translate(Vector3.forward);
+    }
+}
