d046dcd05861c57aedd855ebd67abd01ea9da7d3
raulbarberosanchez <raul.barbero.sanchez@alumnos.upm.es>
2019-05-14 14:23:10 +0200

    texto 3d bien hecho no como piñita que no sabe hacer nada el pesado y va de sobrado por la vida el muy subnormal

---
+++ Assets/Scripts/show3dText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class show3dText : MonoBehaviour
+{
+    GameObject text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GameObject.FindWithTag("texto3d");
+        text.SetActive(!text.activeInHierarchy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.I)) {
+            text.SetActive(!text.activeInHierarchy);
+        }
+    }
+}
